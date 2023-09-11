7d7089fab6e1f35126041f04b83ccd7638a7e6e4
hambone-the-Great <marcus.schuff@gmail.com>
2023-09-11 11:51:55 -0700

    updated assembly info. Trying to publish.

--- PDFConverter/Properties/AssemblyInfo.cs
+++ PDFConverter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PDFConverter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PDF Converter")]
+[assembly: AssemblyDescription("An open source program that converts PDFs from XPS to PDF.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PDFConverter")]
+[assembly: AssemblyCompany("Schuff Software Company")]
+[assembly: AssemblyProduct("PDF Converter")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1349d462-b32e-4af5-98ae-f781601c26e4")]
